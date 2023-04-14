774f94bc633258054cc8415ae9a1ceae2d0fc1e2
mzeenakamura <mzeenakamura@nakamuretanoair>
2023-04-14 12:57:26 +0900

    task_4 一時コミット

--- src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
+++ src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import jp.co.seattle.library.dto.BookDetailsInfo;
 
@@ -29,13 +30,19 @@ public class BookUtil {
 		//TODO　各チェックNGの場合はエラーメッセージをリストに追加（タスク４）
 		List<String> errorList = new ArrayList<>();
 		// 必須チェック
+		if (isEmptyBookInfo(bookInfo)) {
+			errorList.add(REQUIRED_ERROR);
+        }
+        
+        // ISBNのバリデーションチェック
+		if(isValidIsbn(bookInfo.getIsbn())) {
+        errorList.add(ISBN_ERROR);
+		}
 
-		
-		// ISBNのバリデーションチェック
-
-
-		// 出版日の形式チェック
-
+        // 出版日の形式チェック
+		if(!(checkDate(bookInfo.getPublishDate()))) {
+        errorList.add(PUBLISHDATE_ERROR);
+		}
 
 		return errorList;
 	}
@@ -51,7 +58,7 @@ public class BookUtil {
 			DateFormat formatter = new SimpleDateFormat("yyyyMMdd");
 			formatter.setLenient(false); // ←これで厳密にチェックしてくれるようになる
 			//TODO　取得した日付の形式が正しければtrue（タスク４）
-			
+			formatter.parse(publishDate);
 			return true;
 		} catch (Exception p) {
 			p.printStackTrace();
@@ -67,8 +74,15 @@ public class BookUtil {
 	 */
 	private static boolean isValidIsbn(String isbn) {
 		//TODO　ISBNが半角数字で10文字か13文字であればtrue（タスク４）
-		
-		return true;
+		if(isbn.length() > 0) {
+		   if((isbn.length() == 10 || isbn.length() == 13) && isbn.matches("^[0-9]+$")) {
+		     return true;
+	       }else {
+		     return false;
+	       }
+	    }else {
+           return false;
+	    }
 	}
 
 	/**
@@ -79,7 +93,12 @@ public class BookUtil {
 	 */
 	private static boolean isEmptyBookInfo(BookDetailsInfo bookInfo) {
 		//TODO　タイトル、著者、出版社、出版日のどれか一つでもなかったらtrue（タスク４）
-		
-		return true;
+		if (StringUtils.isEmpty(bookInfo.getTitle()) || StringUtils.isEmpty(bookInfo.getAuthor()) ||
+				StringUtils.isEmpty(bookInfo.getPublisher()) || StringUtils.isEmpty(bookInfo.getPublishDate())) {
+            return true;
+         } else {
+            return false;
+         }
+	    
 	}
 }
--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,7 +33,7 @@ public class BooksService {
 
 		// TODO 書籍名の昇順で書籍情報を取得するようにSQLを修正（タスク３）
 		List<BookInfo> getedBookList = jdbcTemplate.query(
-				"",
+				"SELECT * FROM books ORDER BY title ASC;",
 				new BookInfoRowMapper());
 
 		return getedBookList;
@@ -61,7 +61,7 @@ public class BooksService {
 	 */
 	public int registBook(BookDetailsInfo bookInfo) {
 		// TODO 取得した書籍情報を登録し、その書籍IDを返却するようにSQLを修正（タスク４）
-		String sql = "";
+		String sql = "INSERT INTO books (title, author, publisher, publish_date, isbn, description, thumbnail_url, thumbnail_name, reg_date, upd_date)VALUES(?, ?, ?, ?, ?, ?, ?, ?, now(), now()) RETURNING id";
 
 		int bookId = jdbcTemplate.queryForObject(sql, int.class, bookInfo.getTitle(), bookInfo.getAuthor(),
 				bookInfo.getPublisher(), bookInfo.getPublishDate(), bookInfo.getThumbnailName(),
